362ba6778ddd85a27a54e9c2ac67cf6aef859f99
miloszmisiek <milosz.misiek@gmail.com>
2022-01-25 11:40:39 +0000

    Add Savings class with attribiute and calculate_savings method. Check for proper function.

--- run.py
+++ run.py
@@ -12,8 +12,23 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('personal-budget')
 
-general = SHEET.worksheet('general')
 
-data = general.get_all_values()
 
-print(data)
+class Savings:
+    def __init__(self, plan, income):
+        self.plan = plan
+        self.income = income
+
+    def calculate_savings(self):
+        try:
+            if self.plan == "50/30/20":
+                return self.income * 0.2
+            elif self.plan == "70/20/10":
+                return self.income * 0.1
+            else:
+                raise TypeError("Incorrect type for plan argument")
+
+save = Savings("7/20/10", 3000)
+calc_save = save.calculate_savings()
+print(calc_save)
+
