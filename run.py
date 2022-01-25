74e0c65e385d4003e8386593022ea475f5d02d87
miloszmisiek <milosz.misiek@gmail.com>
2022-01-25 14:42:00 +0000

    Add mixin to handle teminal clear operations after functions are called. Define functions for user inputs for budget plan, income and currency

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import pyinputplus as pyip
+import os
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,24 +15,62 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('personal-budget')
 
 
-class Budget:
-    def __init__(self, plan, income, currency):
-        self.plan = plan
-        self.income = income
-        self.currency = currency
-    
+class ClearDisplayMixin:
+    def clear_display(self):
+        os.system('clear')
 
-class Savings(Budget):
+class Budget(ClearDisplayMixin):
+    """
+    Budget class that handles user option for calculations.
+    """
+    def __init__(self):
+        self.plan = self.choose_budget_plan()
+        self.income = self.enter_income()
+        self.currency = self.choose_currency()
+    
+    def choose_budget_plan(self):
+        """
+        Gets user input for budget plan based on menu presented on the screen, validates the choice, clears terinal and returns user's choice.
+        """
+        response = pyip.inputMenu(['50/30/20', '70/20/10', 'About plans'], numbered=True)
+        self.clear_display()
+        return response
+
+    def enter_income(self):
+        """
+        Gets user's input for income, validates the choice, clears terinal and returns user's choice.
+        """
+        income = pyip.inputFloat("Enter your monthly income (-TAX): \n")
+        self.clear_display()
+        return income
+    
+    def choose_currency(self):
+        """
+        Gets user's input for currency based on menu presented on the screen, validates the choice, clears terinal and returns user's choice.
+        """
+        currency = pyip.inputMenu(['PLN', 'EUR', 'GBP', 'USD'], prompt="Enter your currency: \n",  numbered=True)
+        self.clear_display()
+        return currency
+
+class Savings(Budget, ClearDisplayMixin):
+    """
+    Budget child class to handle savings calculations.
+    """
 
     def calculate_savings(self):
+        """
+        Calculates how much money is stored as Savings based on user's choice of budget plan.
+        """
         if self.plan == "50/30/20":
             return self.income * 0.2
         elif self.plan == "70/20/10":
             return self.income * 0.1
         else:
             raise TypeError("Incorrect type for plan argument")
+        self.clear_display()
 
 
-save = Savings("50/30/20", 3000)
-calc_save = save.calculate_savings()
-print(calc_save)
+
+save = Savings()
+cal_save = save.calculate_savings()
+print(cal_save)
