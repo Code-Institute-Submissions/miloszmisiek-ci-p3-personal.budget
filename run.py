f5b06c15002b11893798f4bd13743f7e75884d36
miloszmisiek <milosz.misiek@gmail.com>
2022-01-25 13:25:50 +0000

    Add Budget class with attribiutes. Remove __init__ method from Savings class

--- run.py
+++ run.py
@@ -13,22 +13,24 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('personal-budget')
 
 
-
-class Savings:
-    def __init__(self, plan, income):
+class Budget:
+    def __init__(self, plan, income, currency):
         self.plan = plan
         self.income = income
+        self.currency = currency
+    
+
+class Savings(Budget):
 
     def calculate_savings(self):
-        try:
-            if self.plan == "50/30/20":
-                return self.income * 0.2
-            elif self.plan == "70/20/10":
-                return self.income * 0.1
-            else:
-                raise TypeError("Incorrect type for plan argument")
+        if self.plan == "50/30/20":
+            return self.income * 0.2
+        elif self.plan == "70/20/10":
+            return self.income * 0.1
+        else:
+            raise TypeError("Incorrect type for plan argument")
 
-save = Savings("7/20/10", 3000)
+
+save = Savings("50/30/20", 3000)
 calc_save = save.calculate_savings()
 print(calc_save)
-
