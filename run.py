c215ab6282291c6f696028a6fce1874a829056e5
miloszmisiek <milosz.misiek@gmail.com>
2022-01-26 18:09:08 +0000

    Add while loop to choose_budget_plan function to control user option and to print message to terminal when 'about' is selected

--- run.py
+++ run.py
@@ -21,12 +21,25 @@ class ClearDisplayMixin:
     def clear_display(self):
         os.system('clear')
 
+class DistribiuteMoneyMixin:
+    def calculate_money(self, multiplier):
+        """
+        Calculates how much money is stored as Savings based on user's choice of budget plan.
+        """
+        if self.plan_list[0] == "50/30/20":
+            return round(self.income * multiplier, 1)
+        elif self.plan_list[0] == "70/20/10":
+            return round(self.income * multiplier, 1)
+        else:
+            raise TypeError("Incorrect type for plan argument")
+    
+
 class Budget(ClearDisplayMixin):
     """
     Budget class that handles user option for calculations.
     """
     def __init__(self):
-        self.plan = self.choose_budget_plan()[0]
+        self.plan_list = self.choose_budget_plan()
         self.income = self.enter_income()
         # self.currency = self.choose_currency()
         self.clear_display()
@@ -37,15 +50,21 @@ class Budget(ClearDisplayMixin):
         """
         Gets user input for budget plan based on menu presented on the screen, validates the choice, clears terinal and returns user's choice.
         """
-        response = pyip.inputMenu(['50/30/20', '70/20/10', 'About plans'], numbered=True)
-        if response == '50/30/20':
-            needs = 0.5
-            wants = 0.3
-            savings = 0.2
-        elif response == '70/20/10':
-            needs = 0.7
-            wants = 0.2
-            savings = 0.1
+        while True:
+            response = pyip.inputMenu(['50/30/20', '70/20/10', 'About plans'], prompt="Please select which budget plan you choose:\n", numbered=True)
+            if response == '50/30/20':
+                needs = 0.5
+                wants = 0.3
+                savings = 0.2
+                break
+            elif response == '70/20/10':
+                needs = 0.7
+                wants = 0.2
+                savings = 0.1
+                break
+            else:
+                self.clear_display()
+                print("The 50/30/20 rule is a money management technique that divides your income into three categories:\n50% Needs(essentials)\n30% Wants(non-essentials)\n20% Savings.\n\nBy default this app provides following sub-categories:\nNeeds: Housing, Vehicle costs, Insurance, Food and Banking\nWants: Entertaintment, Wellbeing and Travel\n* Savings is what is meant to be left untouched and used only in case there is absolute need for it. It can cover any unexpected costs.\n\nThe 70/20/10 rule is less robust investment type, where the budget is split in proportion:\n70% Needs\n20% Wants\n10% Savings\n")
         return [response, needs, wants, savings]
 
     def enter_income(self):
@@ -72,44 +91,27 @@ class Budget(ClearDisplayMixin):
         SHEET.worksheet(worksheet).update_cell(month_cell.row, month_income.col, value)
         print(f"{column.capitalize()} updated successfully!\n\n")
     
-    
-
-class Savings(Budget, ClearDisplayMixin):
+class Savings(Budget, ClearDisplayMixin, DistribiuteMoneyMixin):
     """
     Budget child class to handle savings calculations.
     """
+    def __init__(self, plan):
+        self.plan = plan
+        self.calc_save = self.calculate_money(self.plan)
+        self.update_worksheet('general', self.calc_save, MONTH_NOW, 'savings')
+        # self.clear_display()
 
-    def calculate_savings(self):
-        """
-        Calculates how much money is stored as Savings based on user's choice of budget plan.
-        """
-        if self.plan == "50/30/20":
-            return round(self.income * 0.2, 1)
-        elif self.plan == "70/20/10":
-            return round(self.income * 0.1, 1)
-        else:
-            raise TypeError("Incorrect type for plan argument")
-        self.clear_display()
-
-class Needs(Budget, ClearDisplayMixin):
+class Needs(Budget, ClearDisplayMixin, DistribiuteMoneyMixin):
     """
     Budget child class to handle Needs calculations.
     """
-
-    def calculate_needs(self):
-        """
-        Calculates how much money is stored as Needs based on user's choice of budget plan.
-        """
-        if self.plan == "50/30/20":
-            return round(self.income * 0.5, 1)
-        elif self.plan == "70/20/10":
-            return round(self.income * 0.7, 1)
-        else:
-            raise TypeError("Incorrect type for plan argument")
-        self.clear_display()
+    def __init__(self, plan):
+        self.plan = plan
+        self.calc_need = self.calculate_money(self.plan)
+        # self.clear_display()
 
 
 
-save = Savings()
-cal_save = save.calculate_savings()
-save.update_worksheet('general', cal_save, MONTH_NOW, 'savings')
+budget = Budget()
+save = Savings(budget.plan_list[3])
+need = Needs()
