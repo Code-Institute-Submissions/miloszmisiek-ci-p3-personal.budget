71b1a5ac675cfbf78f39034cf0602c874f8f3055
miloszmisiek <milosz.misiek@gmail.com>
2022-01-26 12:12:45 +0000

    Edit update worksheet function to include row argument

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('personal-budget')
+MONTH_NOW = datetime.now().strftime('%B').lower()
 
 
 class ClearDisplayMixin:
@@ -29,7 +30,7 @@ class Budget(ClearDisplayMixin):
         self.income = self.enter_income()
         self.currency = self.choose_currency()
         self.clear_display()
-        self.update_worksheet('general', self.income, 'monthly income')
+        self.update_worksheet('general', self.income, MONTH_NOW, 'monthly income')
         
     
     def choose_budget_plan(self):
@@ -53,17 +54,15 @@ class Budget(ClearDisplayMixin):
         currency = pyip.inputMenu(['PLN', 'EUR', 'GBP', 'USD'], prompt="Enter your currency: \n",  numbered=True)
         return currency
 
-    def update_worksheet(self, worksheet, value, column):
+    def update_worksheet(self, worksheet, value, row, column):
         """
         Updates Google Sheet worksheet based on present month, value and column arguments.
         """
-        print(f"Updating {column} in spreadsheet... ")
-        current_month = datetime.now().strftime('%B').lower()
-        months = SHEET.worksheet(worksheet).col_values(1)
-        month_cell = SHEET.worksheet(worksheet).find(current_month)
+        print(f"Updating {column} in spreadsheet...\n")
+        month_cell = SHEET.worksheet(worksheet).find(row)
         month_income = SHEET.worksheet(worksheet).find(column)
         SHEET.worksheet(worksheet).update_cell(month_cell.row, month_income.col, value)
-        print(f"{column.capitalize()} updated successfully!")
+        print(f"{column.capitalize()} updated successfully!\n")
     
     
 
@@ -88,4 +87,4 @@ class Savings(Budget, ClearDisplayMixin):
 
 save = Savings()
 cal_save = save.calculate_savings()
-save.update_worksheet('general', cal_save, 'savings')
+save.update_worksheet('general', cal_save, MONTH_NOW, 'savings')
